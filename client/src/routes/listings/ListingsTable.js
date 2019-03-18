70da9958dc0ba69e54ee5372c2cc7380448a32df
Osipov Vladimir <zouxuoz@gmail.com>
2019-03-18 13:55:11 +0300

    feat(client): add pagination for listings table

--- client/src/routes/listings/ListingsTable.js
+++ client/src/routes/listings/ListingsTable.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { compose } from 'recompose';
+import { compose, withStateHandlers } from 'recompose';
 import * as R from 'ramda';
-import { Paper, Link, Table, Dropdown, Icon, Menu, withModal } from '@8base/boost';
-import { graphql } from 'react-apollo';
+import { Paper, Link, TableBuilder, Dropdown, Icon, Menu, withModal } from '@8base/boost';
+import { Query } from 'react-apollo';
 import { DateTime } from 'luxon';
 
 import * as sharedGraphQL from 'shared/graphql';
@@ -12,101 +12,149 @@ import { ListingEditDialog } from './ListingEditDialog';
 import { ListingShareDialog } from './ListingShareDialog';
 import { ListingDeleteDialog } from './ListingDeleteDialog';
 
-let ListingsTable = ({ listings, openModal, closeModal }) => (
-  <Table>
-    <Table.Header columns="repeat(10, 1fr) 60px">
-      <Table.HeaderCell>Property</Table.HeaderCell>
-      <Table.HeaderCell>Broker</Table.HeaderCell>
-      <Table.HeaderCell>Buyer</Table.HeaderCell>
-      <Table.HeaderCell>Seller</Table.HeaderCell>
-      <Table.HeaderCell>Status</Table.HeaderCell>
-      <Table.HeaderCell>Closing Date</Table.HeaderCell>
-      <Table.HeaderCell>Created At</Table.HeaderCell>
-      <Table.HeaderCell>Updated At</Table.HeaderCell>
-      <Table.HeaderCell>Documents</Table.HeaderCell>
-      <Table.HeaderCell>Price</Table.HeaderCell>
-      <Table.HeaderCell />
-    </Table.Header>
+const LISTINGS_TABLE_COLUMNS = [
+  { name: 'property', title: 'Property', sortEnable: false },
+  { name: 'broker', title: 'Broker', sortEnable: false },
+  { name: 'buyer', title: 'Buyer', sortEnable: false },
+  { name: 'seller', title: 'Seller', sortEnable: false },
+  { name: 'status', title: 'Status' },
+  { name: 'closingDate', title: 'Closing Date' },
+  { name: 'createdAt', title: 'Created At' },
+  { name: 'updatedAt', title: 'Updated At' },
+  { name: 'documents', title: 'Documents', sortEnable: false },
+  { name: 'price', title: 'Price' },
+  { name: 'actions', width: '60px', sortEnable: false },
+];
 
-    <Table.Body loading={ listings.loading } data={ R.pathOr([], ['listingsList', 'items'], listings) } action="Create Listing" onActionClick={() => openModal(ListingCreateDialog.id)}>
-      {
-        (listing) => (
-          <Table.BodyRow columns="repeat(10, 1fr) 60px" key={ listing.id }>
-            <Table.BodyCell>
-              { R.pathOr('Unititled', ['property', 'title'], listing) }
-            </Table.BodyCell>
-            <Table.BodyCell>
-              { R.pathOr('Unititled', ['broker', 'user', 'firstName'], listing) } { R.pathOr('Unititled', ['broker', 'user', 'lastName'], listing) }
-            </Table.BodyCell>
-            <Table.BodyCell>
-              { R.pathOr('Unititled', ['buyer', 'user', 'firstName'], listing) } { R.pathOr('Unititled', ['buyer', 'user', 'lastName'], listing) }
-            </Table.BodyCell>
-            <Table.BodyCell>
-              { R.pathOr('Unititled', ['seller', 'user', 'firstName'], listing) } { R.pathOr('Unititled', ['seller', 'user', 'lastName'], listing) }
-            </Table.BodyCell>
-            <Table.BodyCell>
-              { listing.status }
-            </Table.BodyCell>
-            <Table.BodyCell>
-              {
-                listing.closingDate
-                  ?
-                  DateTime.fromISO(listing.closingDate).toFormat('ff')
-                  :
-                  'none'
-              }
-            </Table.BodyCell>
-            <Table.BodyCell>
-              { DateTime.fromISO(listing.createdAt).toFormat('ff') }
-            </Table.BodyCell>
-            <Table.BodyCell>
-              { DateTime.fromISO(listing.updatedAt).toFormat('ff') }
-            </Table.BodyCell>
-            <Table.BodyCell>
-              <Dropdown defaultOpen={ false }>
-                <Dropdown.Head>
-                  { listing.documents.items.length } documents
-                </Dropdown.Head>
-                <Dropdown.Body pin="right">
-                  <Paper padding="sm">
-                    {
-                      listing.documents.items.map(({ filename, downloadUrl }) => <Link key={ downloadUrl } target="_blank" href={ downloadUrl } size="sm">{ filename }</Link>)
-                    }
-                  </Paper>
-                </Dropdown.Body>
-              </Dropdown>
-            </Table.BodyCell>
-            <Table.BodyCell>
-              { listing.price }
-            </Table.BodyCell>
-            <Table.BodyCell>
-              <Dropdown defaultOpen={ false }>
-                <Dropdown.Head>
-                  <Icon name="Dots" size="sm" color="LIGHT_GRAY2" />
-                </Dropdown.Head>
-                <Dropdown.Body pin="right">
-                  {
-                    ({ closeDropdown }) => (
-                      <Menu>
-                        <Menu.Item onClick={ () => { openModal(ListingEditDialog.id, {  initialValues: listing }); closeDropdown(); } }>Edit</Menu.Item>
-                        <Menu.Item onClick={ () => { openModal(ListingShareDialog.id, { id: listing.id }); closeDropdown(); } }>Share</Menu.Item>
-                        <Menu.Item onClick={ () => { openModal(ListingDeleteDialog.id, { id: listing.id }); closeDropdown(); } }>Delete</Menu.Item>
-                      </Menu>
-                    )
-                  }
-                </Dropdown.Body>
-              </Dropdown>
-            </Table.BodyCell>
-          </Table.BodyRow>
-        )
+class ListingsTable extends React.Component {
+  onActionClick = () => {
+    const { openModal } = this.props;
+
+    openModal(ListingCreateDialog.id)
+  };
+
+  renderCell = (column, data) => {
+    const { openModal } = this.props;
+
+    let rendered = String(data[column.name]);
+
+    switch (column.name) {
+      case 'property': {
+        rendered = R.pathOr('Unititled', ['property', 'title'], data);
+        break;
       }
-    </Table.Body>
-  </Table>
-);
+      case 'broker': 
+      case 'buyer': 
+      case 'seller': {
+        rendered = `${R.pathOr('Unititled', [column.name, 'user', 'firstName'], data)} ${R.pathOr('Unititled', [column.name, 'user', 'lastName'], data)}`;
+        break;
+      }
+      case 'closingDate': {
+        rendered =  data.closingDate ? DateTime.fromISO(data.closingDate).toFormat('ff') : 'none';
+        break;
+      }
+      case 'createdAt':
+      case 'updatedAt': {
+        rendered = DateTime.fromISO(data[column.name]).toFormat('ff');
+        break;
+      }
+      case 'documents': {
+        rendered = (
+          <Dropdown defaultOpen={ false }>
+            <Dropdown.Head>
+              { R.pathOr(0, ['documents', 'items', 'length'], data) } documents
+            </Dropdown.Head>
+            <Dropdown.Body pin="right">
+              <Paper padding="sm">
+                {
+                  R.pathOr([], ['documents', 'items'], data).map(({ filename, downloadUrl }) => (
+                    <Link key={ downloadUrl } target="_blank" href={ downloadUrl } size="sm">{ filename }</Link>
+                  ))
+                }
+              </Paper>
+            </Dropdown.Body>
+          </Dropdown>
+        );
+        break;
+      }
+      case 'actions': {
+        rendered = (
+          <Dropdown defaultOpen={ false }>
+            <Dropdown.Head>
+              <Icon name="More" color="LIGHT_GRAY2" />
+            </Dropdown.Head>
+            <Dropdown.Body pin="right">
+              {
+                ({ closeDropdown }) => (
+                  <Menu>
+                    <Menu.Item onClick={ () => { openModal(ListingEditDialog.id, {  initialValues: data }); closeDropdown(); } }>Edit</Menu.Item>
+                    <Menu.Item onClick={ () => { openModal(ListingShareDialog.id, { id: data.id }); closeDropdown(); } }>Share</Menu.Item>
+                    <Menu.Item onClick={ () => { openModal(ListingDeleteDialog.id, { id: data.id }); closeDropdown(); } }>Delete</Menu.Item>
+                  </Menu>
+                )
+              }
+            </Dropdown.Body>
+          </Dropdown>
+        );
+
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+
+    return rendered;
+  }
+
+  renderTable = ({ data, loading }) => {
+    const { tableState, onChange } = this.props;
+
+    const total = R.pathOr(null, ['listingsList', 'count'], data);
+
+    const tableData = R.pathOr([], ['listingsList', 'items'], data);
+    const finalTableState = R.assocPath(['pagination', 'total'], total, tableState);
+
+    return (
+      <TableBuilder
+        columns={ LISTINGS_TABLE_COLUMNS }
+        data={ tableData }
+        loading={ loading }
+        action="Create Listing"
+        onActionClick={ this.onActionClick }
+        tableState={ finalTableState }
+        onChange={ onChange }
+        renderCell={ this.renderCell }
+        withPagination
+      />
+    );
+  };
+
+  render() {
+    const { tableState } = this.props;
+
+    const skip = (tableState.pagination.page - 1) * tableState.pagination.pageSize;
+    const first = tableState.pagination.pageSize;
+    const orderBy = R.propOr([], 'sort', tableState).map(({ name, order }) => `${name}_${order}`);
+
+    return (
+      <Query query={sharedGraphQL.LISTINGS_LIST_QUERY} variables={{ orderBy, skip, first }}>
+        {this.renderTable}
+      </Query>
+    );
+  }
+}
 
 ListingsTable = compose(
   withModal,
-  graphql(sharedGraphQL.LISTINGS_LIST_QUERY, { name: 'listings' }),
+  withStateHandlers({ tableState: { pagination: { page: 1, pageSize: 20 }}}, {
+    onChange: ({ tableState }) => (value) => ({
+      tableState: {
+        ...tableState,
+        ...value,
+      },
+    })
+  }),
 )(ListingsTable);
 
 export { ListingsTable };
